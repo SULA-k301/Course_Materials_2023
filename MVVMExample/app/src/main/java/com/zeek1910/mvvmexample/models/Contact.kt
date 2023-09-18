8655b247a8fef3e3b8c964012f184bfddfa43de3
Evheniy Piavka <y.piavka@khai.edu>
2023-09-18 11:20:48 +0300

    removed old materials

--- MVVMExample/app/src/main/java/com/zeek1910/mvvmexample/models/Contact.kt
+++
@@ -1,7 +0,0 @@
-package com.zeek1910.mvvmexample.models
-
-data class Contact(
-    val fistName: String,
-    val lastName: String,
-    val phoneNumber:String
-)
--- NavigationComponentExample/app/src/main/java/com/zeek1910/navigationcomponentexample/ItemModel.kt
+++
@@ -1,9 +0,0 @@
-package com.zeek1910.navigationcomponentexample
-
-import java.io.Serializable
-
-data class ItemModel(
-    val name: String,
-    val age: Int,
-    val capacity: Float
-): Serializable
